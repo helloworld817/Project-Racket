44f60b20171493321fb1d94e5384207d081a4f65
Ramtin <ramtinamirkhani914@yahoo.com>
2025-06-26 11:40:39 +0100

    Add files via upload

---
+++ pr1.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(define database (make-parameter '()))
+
+(define band%
+  (class object%
+    ;;constructor
+    (init-field band-name date time venue cost password)
+    ;;add everything to the database 
+    (begin
+      (database
+       (cons this (database))))
+    ;;getter
+    (define/public get-band (λ () band-name))
+    (define/public get-pass (λ () (password)))
+
+    ;;setters
+    (define/public set-venue (λ (v) (set! venue v)))
+    (define/public set-cost (λ (v) (set! cost v)))
+    (define/public set-time (λ (v) (set! time v)))
+    (define/public set-date (λ (v) (set! date v)))
+    (define/public set-pass (λ (v) (set! password v)))
+    
+    (define/public (edit name new-venue new-cost new-pass new-time)
+      (for-each (λ (b)
+                  (cond ((string=? (send b get-band) name) (begin
+                                                            
+                          (if (not(string=? new-venue "")) (send b set-venue new-venue) (void))
+                          (if (number? new-cost) (send b set-cost new-cost) (void))
+                          (if (not(string=? new-time "")) (send b set-time new-time) (void))
+                          (if (not (string=? new-pass "")) (send b set-pass new-pass) (void)))
+                                                           (void))))
+
+                  (database)))
+                                                        
+    (define/public (summary)
+  (format "~a @ ~a on ~a ~a | $~a | pass: ~a"
+          band-name venue date time cost password))                   
+                        
+    (super-new)))
+
+
+
+(define p (new band% [band-name "A"] [date "12/01/2025"] [time "12"] [venue "London"] [cost 12] [password "123"]))
+(define a (new band% [band-name "B"] [date "12/09/2025"] [time "10"] [venue "Paris"] [cost 800] [password "124"]))
+
+(send p edit "A" "Madrid" "" "" "22")
+(map (λ (b) (send b summary)) (database))
